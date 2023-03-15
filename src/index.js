5a8bef5a60e15f760db089f6d854d9d7363343a4
ovchrk <113932934+ovchrk@users.noreply.github.com>
2023-03-15 19:49:46 +0200

    styles markup done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const DEBOUNCE_DELAY = 300;
 const inputRef = document.querySelector('#search-box');
 const countryListRef = document.querySelector('.country-list');
 const countryInfoRef = document.querySelector('.country-info');
+const listItemRef = document.querySelectorAll('.list-item');
+
+countryListRef.style.listStyleType = 'none';
+countryListRef.style.margin = '20px';
+countryListRef.style.fontSize = '20px';
 
 inputRef.addEventListener('input', debounce(onInput, 500));
 
@@ -16,7 +21,13 @@ let inputValue = '';
 function onInput(evt) {
   inputValue = evt.target.value.toLowerCase();
   console.log(inputValue);
-  fetchCountries(inputValue);
+  if (inputValue !== '') {
+    fetchCountries(inputValue);
+  }
+  if (inputValue === '') {
+    countryInfoRef.innerHTML = '';
+    countryListRef.innerHTML = '';
+  }
 }
 // fetchCountries
 function fetchCountries(name) {
@@ -24,6 +35,9 @@ function fetchCountries(name) {
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   )
     .then(response => {
+      if (!response.ok) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      }
       return response.json();
     })
     .then(renderCountryCard)
@@ -32,31 +46,47 @@ function fetchCountries(name) {
     });
 }
 function renderCountryCard(countries) {
-  if (countries.length >= 10) {
+  if (countries.length > 10) {
     Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
+    countryInfoRef.innerHTML = '';
+    countryListRef.innerHTML = '';
   }
   if (countries.length === 1) {
     const countryMarkup = countries
       .flatMap(({ flags, languages, population, capital, name }) => {
-        return `<h1><svg width="40" height="30px"><use href="${
-          flags.svg
-        }" ></use></svg>${name.official}</h1>
-      <ul>
-        <li>Capital: <span>${capital}</span></li>
-        <li>Population: <span>${population}</span></li>
-        <li>Languages: <span>${Object.values(languages)}</span></li>
+        return `<h1 class="title"><img 
+        src="${flags.svg}" 
+        alt="${flags.alt}" 
+        width="40" height="25" />${name.official}</h1>
+      <ul class="country-info__list">
+        <li class="list-item">Capital: <span>${capital}</span></li>
+        <li class="list-item">Population: <span>${population}</span></li>
+        <li class="list-item">Languages: <span>${Object.values(
+          languages
+        )}</span></li>
       </ul>`;
       })
       .join('');
     countryInfoRef.innerHTML = countryMarkup;
     countryListRef.innerHTML = '';
+
+    const countryInfoListRef = document.querySelector('.country-info__list');
+    const titleRef = document.querySelector('.title');
+
+    titleRef.style.marginLeft = '35px';
+    countryInfoListRef.style.listStyleType = 'none';
+    countryInfoListRef.style.margin = '20px';
   }
   if (countries.length < 11 && countries.length > 1) {
     const countriesListMarkup = countries
-      .flatMap(({ flags, languages, population, capital, name }) => {
-        return `<li><svg width="40" height="30px"><use href="${flags.svg}"></use></svg> ${name.official}</li>`;
+      .flatMap(({ flags, name }) => {
+        return `<li class="list-item">
+        <img
+        src="${flags.svg}" 
+        alt="${flags.alt}" 
+        width="30" height="20" /> ${name.official}</li>`;
       })
       .join('');
     countryListRef.innerHTML = countriesListMarkup;
@@ -64,34 +94,3 @@ function renderCountryCard(countries) {
   }
   console.log(countries);
 }
-
-// //Создание карточек с отфильтрованными странами
-// function renderCountryCard(countries) {
-//   const filteredCountries = countryNameFilter(countries);
-//   console.log(filteredCountries);
-
-//   // const countryMarkup = filteredCountries.map(({}))
-
-//   //деструктуризація обєкта кантрі { name, capital, population, flags, languages }
-//   // country
-//   //   .map(({ preview, original, description }) => {
-//   //     return `
-//   //   <div class = "gallery__item">
-//   //   <a class="gallery__link" href="large-image.jpg">
-//   //   <img src = "${preview}"
-//   //   class = "gallery__image"
-//   //   data-source="${original}"
-//   //   alt="${description}">
-//   //   </a>
-//   //   </div>`;
-//   //   })
-//   //   .join('');
-// }
-
-// //Проверка названия на инпут.Фильтрация массива и возврат нового массива со странами удовлетворяющими критерии поиска
-// function countryNameFilter(countries) {
-//   const filteredCountries = countries.filter(country =>
-//     country.name.official.toLowerCase().includes(inputValue)
-//   );
-//   return filteredCountries;
-// }
